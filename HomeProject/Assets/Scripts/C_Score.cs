7ffa6096b2a1fe4c15f980db577729f0424141c7
nearest00 <jinnielovegood08@gmail.com>
2026-06-01 16:55:07 +0900

    0601_1
    HP바, 감소 애니메이션

--- HomeProject/Assets/Scripts/C_Score.cs
+++ HomeProject/Assets/Scripts/C_Score.cs
@@ -7,10 +7,42 @@ public class C_Score : MonoBehaviour
     public float player1Hp = 100f;
     public float player2Hp = 100f;
 
-    public GameObject ResultPanel;
-    public TextMeshProUGUI ResultText;
-    public TextMeshProUGUI LeftText;
-    public TextMeshProUGUI RightText;
+    public Slider player1Slider;
+    public Slider player2Slider;
+
+    float targetPlayer1Hp;
+    float targetPlayer2Hp;
+
+    void Start()
+    {
+        targetPlayer1Hp = player1Hp;
+        targetPlayer2Hp = player2Hp;
+
+        player1Slider.maxValue = player1Hp;
+        player1Slider.value = player1Hp;
+
+        player2Slider.maxValue = player2Hp;
+        player2Slider.value = player2Hp;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(true, 200f);
+        }
+        player1Slider.value = Mathf.MoveTowards(
+            player1Slider.value,
+            targetPlayer1Hp,
+            50f * Time.deltaTime
+        );
+
+        player2Slider.value = Mathf.MoveTowards(
+            player2Slider.value,
+            targetPlayer2Hp,
+            50f * Time.deltaTime
+        );
+    }
 
     public void TakeDamage(bool isPlayer1, float damage)
     {
@@ -18,25 +50,15 @@ public class C_Score : MonoBehaviour
         {
             player1Hp -= damage;
             player1Hp = Mathf.Max(player1Hp, 0f);
-            if (player1Hp == 0)
-            {
-                ResultPanel.SetActive(true);
-                LeftText.text = "Lose";
-                RightText.text = "Win";
-                ResultText.text = "WINNER : Player 2";
-            }
+
+            targetPlayer1Hp = player1Hp;
         }
         else
         {
             player2Hp -= damage;
             player2Hp = Mathf.Max(player2Hp, 0f);
-            if (player2Hp == 0)
-            {
-                ResultPanel.SetActive(true);
-                LeftText.text = "Win";
-                RightText.text = "Lose";
-                ResultText.text = "WINNER : Player 1";
-            }
+
+            targetPlayer2Hp = player2Hp;
         }
     }
 }
