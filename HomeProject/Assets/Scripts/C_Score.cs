c908b5c40c2bd80c524ab904afcffa9ae6b72256
nearest00 <jinnielovegood08@gmail.com>
2026-06-01 15:47:18 +0900

    0601Test
    깃허브가안된다고요

---
+++ HomeProject/Assets/Scripts/C_Score.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class C_Score : MonoBehaviour
+{
+    float PL1Health=100;
+    float PL2Health=100;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
