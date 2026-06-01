5ac8a211e53e5b902b61a4434f87ca600520736a
nearest00 <jinnielovegood08@gmail.com>
2026-06-01 16:34:16 +0900

    0601_0
    아무일도없었다...
    체력 0이면 패널 / 승자 표시

--- HomeProject/Assets/Scripts/C_Score.cs
+++ HomeProject/Assets/Scripts/C_Score.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class C_Score : MonoBehaviour
 {
     public float player1Hp = 100f;
     public float player2Hp = 100f;
+
+    public GameObject ResultPanel;
+    public TextMeshProUGUI ResultText;
+    public TextMeshProUGUI LeftText;
+    public TextMeshProUGUI RightText;
 
     public void TakeDamage(bool isPlayer1, float damage)
     {
@@ -11,11 +18,25 @@ public class C_Score : MonoBehaviour
         {
             player1Hp -= damage;
             player1Hp = Mathf.Max(player1Hp, 0f);
+            if (player1Hp == 0)
+            {
+                ResultPanel.SetActive(true);
+                LeftText.text = "Lose";
+                RightText.text = "Win";
+                ResultText.text = "WINNER : Player 2";
+            }
         }
         else
         {
             player2Hp -= damage;
             player2Hp = Mathf.Max(player2Hp, 0f);
+            if (player2Hp == 0)
+            {
+                ResultPanel.SetActive(true);
+                LeftText.text = "Win";
+                RightText.text = "Lose";
+                ResultText.text = "WINNER : Player 1";
+            }
         }
     }
 }
